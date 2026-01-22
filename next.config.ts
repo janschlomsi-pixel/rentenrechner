1f84e4950af7ca9aefa9a96100bdaa4c9bd05007
janschlomsi-pixel <janschlomsi@gmail.com>
2026-01-22 17:54:18 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
+const repo = "rentenrechner";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
+
